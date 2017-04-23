594a35a4f74eda448d7c177d6f1bb49067894bda
NewMax <zhangpengmax@gmail.com>
2017-04-22 21:26:52 -0400

    step 7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,11 +4,12 @@ class Job < ApplicationRecord
     self.save
   end
 
+  has_many :resumes
+
   def hide!
     self.is_hidden = true
     self.save
+  end
     scope :published, -> { where(is_hidden: false) }
     scope :recent, -> { order('created_at DESC') }
-
   end
-end
